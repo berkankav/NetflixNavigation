6554e9ecf605ba163947404358f7ad1516d81a87
berkankav <60316646+berkankav@users.noreply.github.com>
2022-07-30 21:56:15 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let open_btn = document.querySelector(".open-btn");
+let close_btn = document.querySelector(".close-btn");
+let nav = document.querySelectorAll(".nav");
+
+open_btn.addEventListener("click", function () {
+    nav.forEach(nav_el => nav_el.classList.add("visible"));
+})
+
+close_btn.addEventListener("click", function () {
+    nav.forEach(nav_el => nav_el.classList.remove("visible"));
+})
